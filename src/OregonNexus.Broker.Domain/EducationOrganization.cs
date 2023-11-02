de7bb73e7bc8bc79832ebac6386a368be61276c1
Makoa Jacobsen <makoa@makoajacobsen.com>
2023-11-02 03:52:51 +0000

    Add address directly to EdOrg

--- src/OregonNexus.Broker.Domain/EducationOrganization.cs
+++ src/OregonNexus.Broker.Domain/EducationOrganization.cs
@@ -9,10 +9,13 @@ public class EducationOrganization : BaseEntity, IAggregateRoot
     public EducationOrganization? ParentOrganization { get; set; }
     public Guid? ParentOrganizationId { get; set; }
     public Guid? AddressId { get; set; }
-    public Address? Address { get; set; }
     public string Name { get; set; } = default!;
     public string? Number { get; set; } = default!;
     public EducationOrganizationType EducationOrganizationType { get; set; } = EducationOrganizationType.District;
+    public string StreetNumberName { get; set; } = default!;
+    public string City { get; set; } = default!;
+    public string StateAbbreviation { get; set; } = default!;
+    public string PostalCode { get; set; } = default!;
 }
 
 public enum EducationOrganizationType
