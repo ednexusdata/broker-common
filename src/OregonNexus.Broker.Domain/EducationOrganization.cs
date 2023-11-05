9c1f279a700f30f109676724d9a1acdf282d8062
Makoa Jacobsen <makoa@makoajacobsen.com>
2023-11-05 00:47:10 +0000

    Update EdOrgAddress fields to nullable

--- src/OregonNexus.Broker.Domain/EducationOrganization.cs
+++ src/OregonNexus.Broker.Domain/EducationOrganization.cs
@@ -8,14 +8,13 @@ public class EducationOrganization : BaseEntity, IAggregateRoot
 
     public EducationOrganization? ParentOrganization { get; set; }
     public Guid? ParentOrganizationId { get; set; }
-    public Guid? AddressId { get; set; }
     public string Name { get; set; } = default!;
     public string? Number { get; set; } = default!;
     public EducationOrganizationType EducationOrganizationType { get; set; } = EducationOrganizationType.District;
-    public string StreetNumberName { get; set; } = default!;
-    public string City { get; set; } = default!;
-    public string StateAbbreviation { get; set; } = default!;
-    public string PostalCode { get; set; } = default!;
+    public string? StreetNumberName { get; set; }
+    public string? City { get; set; }
+    public string? StateAbbreviation { get; set; }
+    public string? PostalCode { get; set; }
 }
 
 public enum EducationOrganizationType
