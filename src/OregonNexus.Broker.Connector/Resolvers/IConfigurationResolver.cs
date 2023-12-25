44c2ad1ffaba0edf41cec4965626d45204a4a015
Makoa Jacobsen <makoa@makoajacobsen.com>
2023-12-25 19:55:51 +0000

    Add configuration resolver for connectors to call

---
+++ src/OregonNexus.Broker.Connector/Resolvers/IConfigurationResolver.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OregonNexus.Broker.Connector.Payload;
+using OregonNexus.Broker.Connector.PayloadContentTypes;
+using OregonNexus.Broker.Domain;
+using OregonNexus.Broker.Domain.Specifications;
+using OregonNexus.Broker.SharedKernel;
+
+namespace OregonNexus.Broker.Connector.Resolvers;
+
+public interface IConfigurationResolver
+{
+    public Task<T> FetchConnectorSettingsAsync<T>();
+}
