309458727f88b7e31b2c2ba6c413693b43500b71
Makoa Jacobsen <makoa@makoajacobsen.com>
2024-02-10 22:54:00 +0000

    Save entire payloadcontent

--- src/OregonNexus.Broker.Connector/PayloadContentType/DocumentContentType.cs
+++
@@ -1,6 +0,0 @@
-namespace OregonNexus.Broker.Connector.PayloadContentTypes;
-
-public abstract class DocumentContentType : PayloadContentType
-{
-
-}
--- src/OregonNexus.Broker.Connector/PayloadContentType/PayloadContentType.cs
+++ src/OregonNexus.Broker.Connector/PayloadContentType/PayloadContentType.cs
@@ -3,7 +3,9 @@ namespace OregonNexus.Broker.Connector.PayloadContentTypes;
 public abstract class PayloadContentType
 {
     public abstract string ContentType { get; }
-    public abstract string Content { get; }
+    public abstract string Schema { get; }
+    public abstract string SchemaVersion { get; }
+    public object? Content { get; set; }
 
     public bool AllowMultiple = false;
 }
