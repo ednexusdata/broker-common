62aa2d3cf2fbb813e25cdd5fb44670ea295ecf17
Makoa Jacobsen <makoa@makoajacobsen.com>
2024-05-05 17:31:13 +0000

    Add JobId to make payload jobs distinct

--- src/EdNexusData.Broker.Domain/PayloadSettings/PayloadSettingsContentType.cs
+++ src/EdNexusData.Broker.Domain/PayloadSettings/PayloadSettingsContentType.cs
@@ -1,12 +1,10 @@
 // Copyright: 2023 Education Nexus Oregon
 // Author: Makoa Jacobsen, makoa@makoajacobsen.com
-
-using System.Text.Json;
-
 namespace EdNexusData.Broker.Domain;
 
 public class PayloadSettingsContentType
 {
+    public Guid JobId { get; set; }
     public string PayloadContentType { get; set; } = default!;
     public string? Settings { get; set; }
 }
