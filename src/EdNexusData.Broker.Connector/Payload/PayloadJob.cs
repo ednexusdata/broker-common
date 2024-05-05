e1627434a6dff67ad78fe645d6d447a3817833e4
Makoa Jacobsen <makoa@makoajacobsen.com>
2024-05-05 16:22:43 +0000

    Change to property

--- src/EdNexusData.Broker.Connector/Payload/PayloadJob.cs
+++ src/EdNexusData.Broker.Connector/Payload/PayloadJob.cs
@@ -2,9 +2,12 @@ namespace EdNexusData.Broker.Connector;
 
 public abstract class PayloadJob
 {
-    public static bool AllowMultiple = false;
+    //public static bool AllowConfiguration = false;
+    
+    //public static bool AllowMultiple = false;
 
-    public static bool AllowConfiguration = false;
+    public virtual bool AllowConfiguration { get; set; } = false;
+    public virtual bool AllowMultiple { get; set; } = false;
     
     public abstract Task<object?> ExecuteAsync(string studentUniqueId);
 }
