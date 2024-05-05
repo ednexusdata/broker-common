9ee1c8aaf2f118e92a5634c2b34da91ceae29cc7
Makoa Jacobsen <makoa@makoajacobsen.com>
2024-05-05 16:52:45 +0000

    Change back to static fields

--- src/EdNexusData.Broker.Connector/Payload/PayloadJob.cs
+++ src/EdNexusData.Broker.Connector/Payload/PayloadJob.cs
@@ -2,12 +2,9 @@ namespace EdNexusData.Broker.Connector;
 
 public abstract class PayloadJob
 {
-    //public static bool AllowConfiguration = false;
+    public static bool AllowConfiguration = false;
     
-    //public static bool AllowMultiple = false;
-
-    public virtual bool AllowConfiguration { get; set; } = false;
-    public virtual bool AllowMultiple { get; set; } = false;
+    public static bool AllowMultiple = false;
     
     public abstract Task<object?> ExecuteAsync(string studentUniqueId);
 }
