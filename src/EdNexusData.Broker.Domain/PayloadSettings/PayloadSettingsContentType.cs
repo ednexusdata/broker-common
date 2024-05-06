b5695bfcd0ccf49d0ec326079ab8ece5a7a279ba
Makoa Jacobsen <makoa@makoajacobsen.com>
2024-05-06 04:16:04 +0000

    JsonDocument didn't work

--- src/EdNexusData.Broker.Domain/PayloadSettings/PayloadSettingsContentType.cs
+++ src/EdNexusData.Broker.Domain/PayloadSettings/PayloadSettingsContentType.cs
@@ -8,5 +8,5 @@ public class PayloadSettingsContentType
 {
     public Guid JobId { get; set; }
     public string PayloadContentType { get; set; } = default!;
-    public JsonDocument? Settings { get; set; }
+    public string? Settings { get; set; }
 }
