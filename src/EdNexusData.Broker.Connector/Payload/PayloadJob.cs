8699d109aece55c2d946614352b23554d55e9447
Makoa Jacobsen <makoa@makoajacobsen.com>
2024-08-24 16:35:24 +0000

    Add start and continue to payload job

--- src/EdNexusData.Broker.Connector/Payload/PayloadJob.cs
+++ src/EdNexusData.Broker.Connector/Payload/PayloadJob.cs
@@ -8,5 +8,15 @@ public abstract class PayloadJob
     
     public static bool AllowMultiple = false;
     
-    public abstract Task<object?> ExecuteAsync(string studentUniqueId, JsonDocument? configuration);
+    public abstract Task<(Status Status, JsonDocument? Resubmit)> StartAsync(string studentUniqueId, JsonDocument? configuration);
+
+    public abstract Task<(Status Status, JsonDocument? Resubmit)> ContinueAsync(JsonDocument? resubmit, string? studentUniqueId, JsonDocument? configuration);
+
+    public abstract Task<object?> FinishAsync(string studentUniqueId, JsonDocument? configuration);
+
+    public enum Status
+    {
+        Continue,
+        Finish
+    }
 }
