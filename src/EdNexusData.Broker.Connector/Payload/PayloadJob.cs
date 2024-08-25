fc305c4f8090b446415f3f3f4ed10b05db972da4
Makoa Jacobsen <makoa@makoajacobsen.com>
2024-08-25 11:27:12 -0700

    Separate delayed from regular

--- src/EdNexusData.Broker.Connector/Payload/PayloadJob.cs
+++ src/EdNexusData.Broker.Connector/Payload/PayloadJob.cs
@@ -7,21 +7,6 @@ public abstract class PayloadJob
     public static bool AllowConfiguration = false;
     
     public static bool AllowMultiple = false;
-
-    protected string? StudentUniqueId;
     
-    protected JsonDocument? Configuration;
-
-    public abstract Task<Status> StartAsync(string studentUniqueId, JsonDocument? configuration);
-
-    public abstract Task<Status> ContinueAsync();
-
-    public abstract Task<object?> FinishAsync();
-
-    public enum Status
-    {
-        Continue,
-        ContinueLater,
-        Finish
-    }
+    public abstract Task<object?> ExecuteAsync(string studentUniqueId, JsonDocument? configuration);
 }
