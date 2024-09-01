b3357ddfa23d8386da1cfb4d2628e77f4721f62c
Makoa Jacobsen <makoa@makoajacobsen.com>
2024-09-01 10:00:22 -0700

    Change to concrete type for generic files

--- src/EdNexusData.Broker.Connector/Payload/DocumentPayloadContent.cs
+++ src/EdNexusData.Broker.Connector/Payload/DocumentPayloadContent.cs
@@ -1,6 +1,6 @@
 namespace EdNexusData.Broker.Connector;
 
-public abstract class DocumentPayloadContent : PayloadContent
+public class DocumentPayloadContent : PayloadContent
 {   
     public string ContentType { get; set; } = default!;
     public string FileName { get; set; } = default!;
