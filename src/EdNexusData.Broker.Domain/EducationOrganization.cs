f07fb3173561895814ccff7feba42ff0f9e9ba3e
Makoa Jacobsen <makoa@makoajacobsen.com>
2024-11-10 16:38:46 +0000

    Add shortname

--- src/EdNexusData.Broker.Domain/EducationOrganization.cs
+++ src/EdNexusData.Broker.Domain/EducationOrganization.cs
@@ -1,8 +1,5 @@
 // Copyright: 2023 Education Nexus Oregon
 // Author: Makoa Jacobsen, makoa@makoajacobsen.com
-
-using System.Text.Json.Serialization;
-
 namespace EdNexusData.Broker.Domain;
 
 public class EducationOrganization : BaseEntity, IAggregateRoot
@@ -10,6 +7,7 @@ public class EducationOrganization : BaseEntity, IAggregateRoot
     public EducationOrganization? ParentOrganization { get; set; }
     public Guid? ParentOrganizationId { get; set; }
     public string Name { get; set; } = default!;
+    public string ShortName { get; set; } = default!;
     public string? Number { get; set; } = default!;
     public EducationOrganizationType EducationOrganizationType { get; set; } = EducationOrganizationType.District;
     public Address? Address { get; set; }
