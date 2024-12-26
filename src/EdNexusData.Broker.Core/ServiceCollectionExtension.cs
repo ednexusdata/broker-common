8dac18c28ec6b06d9c6df29f9a89815e41039f35
Makoa Jacobsen <makoa@makoajacobsen.com>
2024-12-26 21:27:12 +0000

    Move loader to service

--- src/EdNexusData.Broker.Core/ServiceCollectionExtension.cs
+++ src/EdNexusData.Broker.Core/ServiceCollectionExtension.cs
@@ -1,40 +1,40 @@
-using EdNexusData.Broker.Core.Connector;
+// using EdNexusData.Broker.Core.Connector;
 
-namespace Microsoft.Extensions.DependencyInjection;
+// namespace Microsoft.Extensions.DependencyInjection;
 
-public static class MyConfigServiceCollectionExtensions
-{
-    /*
-    public static IServiceCollection AddConfig(this IServiceCollection services, IConfiguration config)
-    {
-        return services;
-    }
-    */
+// public static class MyConfigServiceCollectionExtensions
+// {
+//     /*
+//     public static IServiceCollection AddConfig(this IServiceCollection services, IConfiguration config)
+//     {
+//         return services;
+//     }
+//     */
 
-    public static IServiceCollection AddConnectorLoader(this IServiceCollection services)
-    {
-        services.AddSingleton<ConnectorLoader>();
+//     public static IServiceCollection AddConnectorLoader(this IServiceCollection services)
+//     {
+//         services.AddSingleton<ConnectorLoader>();
         
-        return services;
-    }
+//         return services;
+//     }
     
-    public static IServiceCollection AddConnectorDependencies(this IServiceCollection services)
-    {
-        Activator.CreateInstance<ConnectorLoader>();
+//     public static IServiceCollection AddConnectorDependencies(this IServiceCollection services)
+//     {
+//         Activator.CreateInstance<ConnectorLoader>();
         
-        //services.AddSingleton<AuthenticationProvidersLocator>();
-        //services.AddScoped<ConnectorCredentialRetriever>();
+//         //services.AddSingleton<AuthenticationProvidersLocator>();
+//         //services.AddScoped<ConnectorCredentialRetriever>();
 
-        var types = AppDomain.CurrentDomain.GetAssemblies()
-                        .SelectMany(s => s.GetExportedTypes())
-                        .Where(p => p.GetInterface(nameof(IConnectorServiceCollection)) is not null);
+//         var types = AppDomain.CurrentDomain.GetAssemblies()
+//                         .SelectMany(s => s.GetExportedTypes())
+//                         .Where(p => p.GetInterface(nameof(IConnectorServiceCollection)) is not null);
         
-        foreach(var type in types)
-        {   
-            var myMethod = type.GetMethod("AddDependencies");
-            myMethod!.Invoke(null, new object[] { services });
-        }
+//         foreach(var type in types)
+//         {   
+//             var myMethod = type.GetMethod("AddDependencies");
+//             myMethod!.Invoke(null, new object[] { services });
+//         }
 
-        return services;
-    }
-}
+//         return services;
+//     }
+// }
