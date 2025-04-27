9fd8a8e6d8a934056fb66b2a000b8b3fc7bee174
Makoa Jacobsen <makoa@makoajacobsen.com>
2025-04-27 16:26:50 +0000

    Add EdOrg for record to remove focus req

--- src/Configuration/IConfigurationResolver.cs
+++ src/Configuration/IConfigurationResolver.cs
@@ -2,6 +2,8 @@ namespace EdNexusData.Broker.Common.Configuration;
 
 public interface IConfigurationResolver
 {
+    public Guid? CurrentRecordEducationOrganizationId { get; set; }
+
     public Task<T> FetchConnectorSettingsAsync<T>();
 
     public Task<T> FetchConnectorSettingsAsync<T>(string districtNumber);
